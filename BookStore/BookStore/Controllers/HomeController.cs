9f05ab137ae76022fa3285e0a3e13e0fcbff3551
Robin Varghese <robin.varghese@pl.abb.com>
2023-06-27 19:45:09 +0530

    added mvc to the application

---
+++ BookStore/BookStore/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers {
+  public class HomeController: Controller {
+    public string Index() {
+      return "WebGentle";
+    }
+  }
+}
--- BookStore/BookStore/Startup.cs
+++ BookStore/BookStore/Startup.cs
@@ -13,6 +13,8 @@ namespace BookStore {
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices( IServiceCollection services ) {
+
+      services.AddControllersWithViews();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,9 +26,7 @@ namespace BookStore {
       app.UseRouting();
 
       app.UseEndpoints( endpoints => {
-        endpoints.MapGet( "/", async context => {
-          await context.Response.WriteAsync( "Hello World!" );
-        } );
+        endpoints.MapDefaultControllerRoute();
       } );
     }
   }
